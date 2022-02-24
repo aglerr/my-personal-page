e40cf1be5996ae11e3426f177b1f581a438166ab
aglerr <muhammaddaffa912@gmail.com>
2022-02-24 15:02:30 +0700

    Initial commit

---
+++ resources/views/pages/sidebar.blade.php
@@ -0,0 +1,49 @@
+<header id="header">
+    <div class="d-flex flex-column">
+
+        <div class="profile">
+            <img src="assets/img/logo-white-bg.png">
+        </div>
+
+        <nav id="navbar" class="nav-menu navbar">
+            <ul>
+                <li>
+                    <a href="#hero" class="nav-link scrollto active">
+                        <i class="bx bx-home"></i>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#about" class="nav-link scrollto">
+                        <i class="bx bx-user"></i>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#resume" class="nav-link scrollto">
+                        <i class="bx bx-file-blank"></i>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#portfolio" class="nav-link scrollto">
+                        <i class="bx bx-book-content"></i>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#services" class="nav-link scrollto">
+                        <i class="bx bx-server"></i>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#contact" class="nav-link scrollto">
+                        <i class="bx bx-envelope"></i>
+                    </a>
+                </li>
+
+            </ul>
+        </nav><!-- .nav-menu -->
+    </div>
+</header><!-- End Header -->
