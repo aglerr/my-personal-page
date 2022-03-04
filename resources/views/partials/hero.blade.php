9081d01f1150d7882a61630f2036825efbc17372
aglerr <muhammaddaffa912@gmail.com>
2022-03-04 20:53:04 +0700

    xxx

---
+++ resources/views/partials/hero.blade.php
@@ -0,0 +1,10 @@
+<section id="hero">
+    <div class="hero-container" data-aos="fade-in">
+        <h1>Muhammad Daffa</h1>
+        <p>I'm a <span class="typed" data-typed-items="Student, Software Developer"></span>
+        </p>
+
+        
+
+    </div>
+</section>
