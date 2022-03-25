83bddacad1b71d39e56769b96721785eabd02257
mdaffa48 <work@muhammaddaffa.com>
2022-03-25 20:47:33 +0700

    Add new portfolio section above footer

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -29,7 +29,7 @@
     <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
         integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-        
+
     <!-- Template Main CSS File -->
     <link href="assets/css/style.css" rel="stylesheet">
 
@@ -56,13 +56,19 @@
         @include('partials.about')
         @include('partials.skills')
         @include('partials.resume')
-        {{-- @include('partials.portfolio.portfolio') --}}
         @include('partials.projects')
         @include('partials.contact')
+
+        {{-- Portfolio section --}}
+        <a href="/portfolio">
+            <section id="portfolio-hero" class="portfolio-hero">
+                <h1>Click to see my portfolio</h1>
+            </section>
+        </a>
+
         @include('partials.footer')
 
     </main><!-- End #main -->
-
 
 
     <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
