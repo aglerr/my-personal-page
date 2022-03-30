fc83dd9292404f526e4fb787acb55263eb674293
mdaffa48 <work@muhammaddaffa.com>
2022-03-30 19:12:11 +0700

    Now use rsvslider libs

--- resources/views/portfolio/playercosmetics.blade.php
+++ resources/views/portfolio/playercosmetics.blade.php
@@ -30,6 +30,7 @@
 
     <!-- Template Main CSS File -->
     <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('assets/css/rvslider.min.css') }}">
 
     <!-- Rubik Font -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -39,8 +40,66 @@
 </head>
 
 <body>
+    <div class="portfolio-details">
 
-    <section id="portfolio-details" class="portfolio-details section-bg">
+        <div class="container">
+            
+            <div class="section-title">
+                <h2><a href="/">Home</a> / <a href="/portfolio">Portfolio</a> / Player Cosmetics</h2>
+            </div>
+
+            <div class="rvs-container rvs-blue-highlight rvs-youtube-play">
+                
+                <div class="rvs-item-container">
+                    <div class="rvs-item-stage">
+                        <div class="rvs-item"
+                            style="background-image: url(https://img.youtube.com/vi/1xCSoe8Nbis/maxresdefault.jpg)">
+                            <p class="rvs-item-text">Preview #1</p>
+                            <a href="https://www.youtube.com/watch?v=1xCSoe8Nbis" class="rvs-play-video"></a>
+                        </div>
+                        <div class="rvs-item"
+                            style="background-image: url(https://img.youtube.com/vi/-L7ep8XuofQ/maxresdefault.jpg)">
+                            <p class="rvs-item-text">Preview #2</p>
+                            <a href="https://www.youtube.com/watch?v=-L7ep8XuofQ" class="rvs-play-video"></a>
+                        </div>
+                        <div class="rvs-item"
+                            style="background-image: url(https://img.youtube.com/vi/13_NjIFef9c/maxresdefault.jpg)">
+                            <p class="rvs-item-text">Preview #3</p>
+                            <a href="https://www.youtube.com/watch?v=13_NjIFef9c" class="rvs-play-video"></a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="rvs-nav-container">
+                    <a class="rvs-nav-prev"></a>
+                    <div class="rvs-nav-stage">
+                        <a class="rvs-nav-item">
+                            <span class="rvs-nav-item-thumb"
+                                style="background-image: url(https://img.youtube.com/vi/1xCSoe8Nbis/maxresdefault.jpg)"></span>
+                            <h4 class="rvs-nav-item-title">Preview #1</h4>
+                            <small class="rvs-nav-item-credits">How plugin works</small>
+                        </a>
+                        <a class="rvs-nav-item">
+                            <span class="rvs-nav-item-thumb"
+                                style="background-image: url(https://img.youtube.com/vi/-L7ep8XuofQ/maxresdefault.jpg)"></span>
+                            <h4 class="rvs-nav-item-title">Preview #2</h4>
+                            <small class="rvs-nav-item-credits">Colorable Cosmetics</small>
+                        </a>
+                        <a class="rvs-nav-item">
+                            <span class="rvs-nav-item-thumb"
+                                style="background-image: url(https://img.youtube.com/vi/13_NjIFef9c/maxresdefault.jpg)"></span>
+                            <h4 class="rvs-nav-item-title">Preview #3</h4>
+                            <small class="rvs-nav-item-credits">Wardrobe Feature</small>
+                        </a>
+                    </div>
+                    <a class="rvs-nav-next"></a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    {{-- <section id="portfolio-details" class="portfolio-details section-bg">
         <div class="container">
 
             <div class="section-title">
@@ -52,29 +111,24 @@
                 <div class="col-sm-6 col-lg-4 mb-3" style="width: 750px;">
                     <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel"
                         data-bs-interval="false">
-                        {{-- <div class="carousel-indicators">
-                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0"
-                                class="active" aria-current="true" aria-label="Slide 1"></button>
-                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
-                                aria-label="Slide 2"></button>
-                        </div> --}}
+
                         <div class="carousel-inner">
-                            <div class="carousel-item active" style="width: 750px; height: 375px;">
-                                <iframe class="d-block w-100" width="750" height="375"
-                                    src="https://www.youtube.com/embed/1xCSoe8Nbis?controls=1"
-                                    title="YouTube video player" frameborder="0" allowfullscreen></iframe>
+                            <div class="carousel-item active">
+                                <iframe class="responsive-iframe"
+                                    src="https://www.youtube.com/embed/1xCSoe8Nbis?controls=1" frameborder="0"></iframe>
                             </div>
-                            <div class="carousel-item" style="width: 750px; height: 375px;">
-                                <iframe class="d-block w-100" width="750" height="375"
+                            <div class="carousel-item">
+                                <iframe class="responsive-iframe d-block w-100"
                                     src="https://www.youtube.com/embed/-L7ep8XuofQ?controls=1"
                                     title="YouTube video player" frameborder="0" allowfullscreen></iframe>
                             </div>
-                            <div class="carousel-item" style="width: 750px; height: 375px;">
-                                <iframe class="d-block w-100" width="750" height="375"
+                            <div class="carousel-item">
+                                <iframe class="responsive-iframe d-block w-100"
                                     src="https://www.youtube.com/embed/13_NjIFef9c?controls=1"
                                     title="YouTube video player" frameborder="0" allowfullscreen></iframe>
                             </div>
                         </div>
+
                         <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
                             data-bs-slide="prev">
                             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -107,7 +161,7 @@
             </div>
 
         </div>
-    </section>
+    </section> --}}
 
     <!-- Vendor JS Files -->
     <script src="{{ asset('assets/vendor/purecounter/purecounter.js') }}"></script>
@@ -119,9 +173,17 @@
     <script src="{{ asset('assets/vendor/typed.js/typed.min.js') }}"></script>
     <script src="{{ asset('assets/vendor/waypoints/noframework.waypoints.js') }}"></script>
     <script src="{{ asset('assets/vendor/php-email-form/validate.js') }}"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 
     <!-- Template Main JS File -->
     <script src="{{ asset('assets/js/main.js') }}"></script>
+    <script src="{{ asset('assets/js/rvslider.min.js') }}"></script>
+
+    <script>
+        jQuery(function($) {
+            $(".rvs-container").rvslider();
+        });
+    </script>
 
 </body>
 
